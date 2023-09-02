5dca4a89161e9b3c9e69e001eb5df3101b029008
Pascal Keller <pascalkeller@gmail.com>
2023-09-02 18:59:35 +0200

    Add implementation infos and start implementing collecting logic

--- CeptoMock/InterceptorSourceGenerator.cs
+++ CeptoMock/InterceptorSourceGenerator.cs
@@ -12,6 +12,26 @@ public class InterceptorSourceGenerator : ISourceGenerator
 
     public void Execute(GeneratorExecutionContext context)
     {
-        Console.WriteLine("Reaching Code Generation");
+        // Collect all Cepto.Mock statements via reflection
+        var ceptoMockStatements = CollectCeptoMockStatements();
+        // Iterate through the statements and analyze the important information:
+        //   Type to Mock
+        //   Method to Mock
+        //   ExpectedInvocationsCount for method of type
+        // These information should be stored in a DTO
+
+        // Iterate through the DTOs to gather location information of the methods to mock:
+        //   Get class and exact line of code in the source code via reflection
+        //   Also add these information to the DTOs
+
+        // Iterate again through the DTOs in order to generate the Interceptor classes
+        //   TODO: More details
+    }
+
+    private IEnumerable<string> CollectCeptoMockStatements()
+    {
+        var ceptoMockStatements = new List<string>();
+
+        return ceptoMockStatements;
     }
 }
---
+++ CeptoMock/MockInformation.cs
@@ -0,0 +1,8 @@
+﻿namespace CeptoMock;
+
+internal class MockInformation
+{
+    public Type TypeToMock { get; set; }
+    public string MethodToMock { get; set; }
+    public int ExpectedInvocationCount { get; set; }
+}
